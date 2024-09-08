6af4b926d445572ff44b264b2e5849e4862b9093
aged99oow <aged99oow@yahoo.co.jp>
2024-09-08 20:58:39 +0900

    add ej_sentence

--- MiniTyping/MiniTyping.py
+++ MiniTyping/MiniTyping.py
@@ -1,6 +1,7 @@
 #
-# MiniTyping.py 2024/2/18
+# MiniTyping.py 2024/9/8
 #
+USE_K8X12_BDF = False
 try:
     import pyttsx3
 except ImportError:
@@ -8,8 +9,10 @@ except ImportError:
 else:
     TTS = True
 import pyxel
-import kfont
-
+if USE_K8X12_BDF:
+    K8X12 = pyxel.Font('./K8X12.bdf')
+else:
+    import kfont
 W_WIDTH, W_HEIGHT = 277, 117
 MSG_X, MSG_Y, MSG_WIDTH, MSG_LINE = 5, 5, W_WIDTH-10, 6
 SMSG_X, SMSG_Y, SMSG_WIDTH, SMSG_HEIGHT = MSG_X+MSG_WIDTH-53, MSG_Y+4, 49, 39
@@ -17,31 +20,15 @@ IN_X, IN_Y, IN_WIDTH, IN_HEIGHT = MSG_X, 84, MSG_WIDTH, 28
 SPK_X, SPK_Y = IN_X+IN_WIDTH-15, IN_Y+10
 FRAME_COL = 12
 NON_WORD = ' -.,\'":;/!?$0123456789'
-PROPER_NOUN = (
-    'Ben', 'Daniel', 'Nancy', 'Greg', 'Kate', 'Robert', 'Richard', 'Angela', 'Arthur', 'Gary', 
-    'Cindy', 'Yoshio', 'Clara', 'Kenji', 'Mary', 'Anne', 'Jack', 'Laura', 'Tom', 'Kent', 
-    'Kazuo', 'Nick', 'John', 'Dorothy', 'Paul', 'Auster', 'Barcelona', 'Denis', 'Hiroko', 'Hiroshi', 
-    'Mayumi', 'Bill', 'Sean', 'Jimmy', 'Tadashi', 'Dennis', 'Smith', 'Harvard', 'Taro', 'Hiromi', 
-    'Emmy', 'Susan', 'Paul', 'Linda', 'Lucy', 'Cinderella', 'Matt', 'Nelson', 'Beth', 'Steve',
-    'Hisashi', 'Dave', 'Yumi', 'Jim', 'Nile', 'Dublin', 'Peru', 'Katherine', 'Akiko', 'karate', 
-    'Miki', 'Brian', 'Sam', 'Celsius', 'Suzuki', 'Sara', 'Tanaka', 'Bob', 'Ginza', 'Hokkaido', 
-    'Los Angeles', 'Lisa', 'karate', 'Ted', 'Sally', 'Kevin', 'Hawaii', 'New York', 'Dick', 'London', 
-    'Tokyo', 'Osaka', 'Nakagawa', 'California', 'Titanic', 'Haruko', 'Asia', 'Australia', 'Hilary', 'Michael', 
-    'South Carolina', 'Ichiro', 'Harry', 'Cathy', 'Sue', 'San Diego', 'Las Vegas', 'Eliza', 'Gifu', 'Brown', 
-    'Keiko', 'New Zealand', 'Amanda', 'CD', 'PC', 'TV', 'Jenny', 'Satsuki', 'Universal Studios Japan', 
-    'Brazil', 'Yuko', 'Mozart', 'Korea', 'Jill', 'Tommy', 'Randy', 'Jones', 'Judy', 'Jane', 'Sherlock Holmes', 
-    'George Orwell', 'Jessica', '', 
-)
 HINT_RATE = 8
 HINT_FIRST_WORD = True
-
 
 class Shooting:
     def __init__(self, x, y, width, height, rate=200):
         self.sx, self.sy, self.width, self.height = x, y, width, height
         self.x1, self.y1, self.x2, self.y2, self.c = 0, 0, 0, 0, 0
         self.rate, self.state = rate, 0
-    
+
     def update(self):
         if self.state==0:
             if pyxel.rndi(1, self.rate)==1:
@@ -69,7 +56,7 @@ class Blinking:
         self.sx, self.sy, self.width, self.height = x, y, width, height
         self.tx, self.ty, self.tc = 0, 0, 0
         self.rate, self.state = rate, 0
-    
+
     def update(self):
         if self.state==0:
             if pyxel.rndi(1, self.rate)==1:
@@ -107,7 +94,7 @@ class Message:
             self.msg_height = height
         self.msg_scrl = 0
         self.clr()
-    
+
     def clr(self):
         self.msg_str = ['']*self.msg_line
         self.msg_col = [7]*self.msg_line
@@ -123,14 +110,20 @@ class Message:
             self.msg_str[0] = new_msg
             self.msg_col[0] = new_col
             self.msg_scrl = 12
-    
+
     def draw_message(self, frcol=7):
         pyxel.rectb(self.msg_x, self.msg_y, self.msg_width, self.msg_height, frcol)
         pyxel.rect(self.msg_x+1, self.msg_y+1, self.msg_width-2, self.msg_height-2, self.msg_bgcol)
         for i in range(1, self.msg_line):
-            kfont.text(self.msg_x+2, self.msg_y+2+(self.msg_line-i-1)*12+self.msg_scrl, self.msg_str[i], self.msg_col[i])
+            if USE_K8X12_BDF:
+                pyxel.text(self.msg_x+2, self.msg_y+2+(self.msg_line-i-1)*12+self.msg_scrl, self.msg_str[i], self.msg_col[i], K8X12)
+            else:
+                kfont.text(self.msg_x+2, self.msg_y+2+(self.msg_line-i-1)*12+self.msg_scrl, self.msg_str[i], self.msg_col[i])
         if self.msg_scrl==0:
-            kfont.text(self.msg_x+2, self.msg_y+2+(self.msg_line-1)*12, self.msg_str[0], self.msg_col[0])
+            if USE_K8X12_BDF:
+                pyxel.text(self.msg_x+2, self.msg_y+2+(self.msg_line-1)*12, self.msg_str[0], self.msg_col[0], K8X12)
+            else:
+                kfont.text(self.msg_x+2, self.msg_y+2+(self.msg_line-1)*12, self.msg_str[0], self.msg_col[0])
 
     def scroll(self):
         if self.msg_scrl > 0:
@@ -147,7 +140,7 @@ class App:
                 spc = False
             ret_hint += ' ' if spc else c
             spc = False if c in NON_WORD else True
-        for w in PROPER_NOUN:  # Proper Noun
+        for w in self.proper_noun:  # Proper Noun
             len_s, len_w = len(self.eng_org), len(w)
             for i in range(len_s-len_w+1):
                 if w==self.eng_org[i:(i+len_w)]:
@@ -172,14 +165,14 @@ class App:
                     find_word = True
                     end_pos = start_pos+1
         return ret_hint
-    
+
     def hint_this_word(self):
         new_pos = self.pos+1
         while new_pos<len(self.eng_org) and not self.eng_low[new_pos] in NON_WORD:
             new_pos += 1
         new_hint = self.hint[:self.pos]+self.eng_org[self.pos:new_pos]+self.hint[new_pos:]
         return new_hint
-        
+
     def set_sentence(self):
         r = pyxel.rndi(0, len(self.eng_jpn)-1)
         self.jpn = self.eng_jpn[r][1]
@@ -204,6 +197,11 @@ class App:
                 l_jpn = f.readline().rstrip()
                 if l_eng[0]!='#' and l_jpn[0]!='#':
                     self.eng_jpn.append([l_eng, l_jpn])
+        self.proper_noun = []
+        with open("proper_noun.txt", "r", encoding="utf-8") as f:
+            while l:=f.readline().rstrip():
+                if l[0]!='#':
+                    self.proper_noun.append(l)
         self.msg = Message(MSG_X, MSG_Y, MSG_WIDTH, MSG_LINE)
         self.blink = Blinking(0, 0, W_WIDTH, W_HEIGHT, 500)
         self.shoot = Shooting(0, 0, W_WIDTH, W_HEIGHT, 1000)
@@ -305,22 +303,37 @@ class App:
     def draw(self):
         pyxel.cls(1)
         self.msg.draw_message(8 if self.shake else FRAME_COL)
-        
+
         pyxel.rectb(IN_X, IN_Y, IN_WIDTH, IN_HEIGHT, 8 if self.shake else FRAME_COL)
         pyxel.rect(IN_X+1, IN_Y+1, IN_WIDTH-2, IN_HEIGHT-2, 0)
-        kfont.text(IN_X+2, IN_Y+3, self.jpn, 7)
-        kfont.text(IN_X+2, IN_Y+15, self.eng_org[:self.pos], 14 if self.sentence_mistake else 11 if self.in_pass else 10)
+        if USE_K8X12_BDF:
+            pyxel.text(IN_X+2, IN_Y+3, self.jpn, 7, K8X12)
+            pyxel.text(IN_X+2, IN_Y+15, self.eng_org[:self.pos], 14 if self.sentence_mistake else 11 if self.in_pass else 10, K8X12)
+        else:
+            kfont.text(IN_X+2, IN_Y+3, self.jpn, 7)
+            kfont.text(IN_X+2, IN_Y+15, self.eng_org[:self.pos], 14 if self.sentence_mistake else 11 if self.in_pass else 10)
         if self.pos<len(self.eng_org):
-            kfont.text(IN_X+2+self.pos*4, IN_Y+15, self.hint[self.pos], 7)
+            if USE_K8X12_BDF:
+                pyxel.text(IN_X+2+self.pos*4, IN_Y+15, self.hint[self.pos], 7, K8X12)
+            else:
+                kfont.text(IN_X+2+self.pos*4, IN_Y+15, self.hint[self.pos], 7)
         if self.pos+1<len(self.eng_org):
-            kfont.text(IN_X+2+(self.pos+1)*4, IN_Y+15, self.hint[self.pos+1:], 6)
-        
+            if USE_K8X12_BDF:
+                pyxel.text(IN_X+2+(self.pos+1)*4, IN_Y+15, self.hint[self.pos+1:], 6, K8X12)
+            else:
+                kfont.text(IN_X+2+(self.pos+1)*4, IN_Y+15, self.hint[self.pos+1:], 6)
+
         pyxel.rectb(SMSG_X, SMSG_Y, SMSG_WIDTH, SMSG_HEIGHT, 8 if self.shake else FRAME_COL)
         pyxel.rect(SMSG_X+1, SMSG_Y+1, SMSG_WIDTH-2, SMSG_HEIGHT-2, 5 if SMSG_X<=pyxel.mouse_x<SMSG_X+SMSG_WIDTH and SMSG_Y<=pyxel.mouse_y<SMSG_Y+SMSG_HEIGHT else 1)
-        kfont.text(SMSG_X+3, SMSG_Y+2, f'Correct{self.total_correct:>4}', 10)
-        kfont.text(SMSG_X+3, SMSG_Y+14, f'Mistake{self.total_mistake:>4}', 14)
-        kfont.text(SMSG_X+3, SMSG_Y+26, f'Pass   {self.total_pass:>4}', 11)
-        
+        if USE_K8X12_BDF:
+            pyxel.text(SMSG_X+3, SMSG_Y+2, f'Correct{self.total_correct:>4}', 10, K8X12)
+            pyxel.text(SMSG_X+3, SMSG_Y+14, f'Mistake{self.total_mistake:>4}', 14, K8X12)
+            pyxel.text(SMSG_X+3, SMSG_Y+26, f'Pass   {self.total_pass:>4}', 11, K8X12)
+        else:
+            kfont.text(SMSG_X+3, SMSG_Y+2, f'Correct{self.total_correct:>4}', 10)
+            kfont.text(SMSG_X+3, SMSG_Y+14, f'Mistake{self.total_mistake:>4}', 14)
+            kfont.text(SMSG_X+3, SMSG_Y+26, f'Pass   {self.total_pass:>4}', 11)
+
         if TTS and self.in_sentence==4:
             pyxel.mouse(False)
             pyxel.trib(SPK_X, SPK_Y+3, SPK_X+5, SPK_Y, SPK_X+5, SPK_Y+6, 10)
